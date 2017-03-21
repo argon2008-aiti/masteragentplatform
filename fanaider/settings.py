37a8efd471dc16cc52a9f2700f408cfa0f7feaed
=argon <yunguta@gmail.com>
2017-03-21 13:28:48 +0000

    downloaded gunicorn

--- fanaider/settings.py
+++ fanaider/settings.py
@@ -131,3 +131,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+
+import dj_database_url
+
+DATABASES["default"] = dj_database_url.config()
