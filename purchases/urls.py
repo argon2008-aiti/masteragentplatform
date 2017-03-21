6941c4b9091c6f0e1f9357e004e9c74fbe8713f6
=argon <yunguta@gmail.com>
2017-03-21 21:09:49 +0000

    added 'purchase metrics' tab

--- purchases/urls.py
+++ purchases/urls.py
@@ -4,5 +4,6 @@ from views import *
 urlpatterns = [
     url(r'^all/$', DayPurchaseListView.as_view(), name="all"),
     url(r'^new/$', NewPurchaseView.as_view(), name="new"),
+    url(r'^metrics/$', NewPurchaseView.as_view(), name="metrics"),
     url(r'^detail/(?P<pk>\d+)/$', DayPurchaseDetail.as_view(), name="detail"),
 ]
