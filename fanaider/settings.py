c7fe3db9099dfb437d9373329dccf148a2af8131
=argon <yunguta@gmail.com>
2017-08-03 02:00:39 +0000

    moved statics files to aws s3

--- fanaider/settings.py
+++ fanaider/settings.py
@@ -137,8 +137,8 @@ USE_L10N = True
 USE_TZ = True
 
 AWS_STORAGE_BUCKET_NAME = 'bucket.masteragentplatform.s3'
-AWS_ACCESS_KEY_ID = 'AKIAJ5CLVUE4GTVAQXBA'
-AWS_SECRET_ACCESS_KEY = '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'
+AWS_ACCESS_KEY_ID = 'AKIAIVGW27QRPENKW6BA'
+AWS_SECRET_ACCESS_KEY = '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'
 
 # Tell django-storages that when coming up with the URL for an item in S3 storage, keep
 # it simple - just use this domain plus the path. (If this isn't set, things get complicated).
