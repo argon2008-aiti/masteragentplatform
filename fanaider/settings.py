b259dc85cded8f07590b4410679c445e1d14baa5
=argon <yunguta@gmail.com>
2017-08-03 01:14:04 +0000

    moved statics files to aws s3

--- fanaider/settings.py
+++ fanaider/settings.py
@@ -144,7 +144,7 @@ AWS_SECRET_ACCESS_KEY = 'HYWNug4Wldy7tobkGOrM1AvA3sSScIzOFhkzdA4Z'
 # it simple - just use this domain plus the path. (If this isn't set, things get complicated).
 # This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
 # We also use it in the next setting.
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_CUSTOM_DOMAIN = 's3.amazonaws.com/%s' % AWS_STORAGE_BUCKET_NAME
 
 # This is used by the `static` template tag from `static`, if you're using that. Or if anything else
 # refers directly to STATIC_URL. So it's safest to always set it.
