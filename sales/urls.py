b00e523c9763f410dac8d298907d7968b4eb8615
argon2008-aiti <macpherson2008@yahoo.com>
2018-09-18 16:10:20 +0000

    Corrected Login redirect url

--- sales/urls.py
+++ sales/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from views import *
 
 urlpatterns = [
-    url(r'^bookings/all$', AllBookingView.as_view(), name="bookings-all"),
+    url(r'^bookings/all/$', AllBookingView.as_view(), name="bookings-all"),
     url(r'^bookings/add/$', NewBookingView.as_view(), name="add-booking"),
     url(r'^metrics/$', NewBookingView.as_view(), name="metrics"),
     url(r'^bookings/edit/(?P<pk>\d+)/$', BookingUpdateView.as_view(), name="edit"),
