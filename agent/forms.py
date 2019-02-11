0dd4c14a90b13b12a14cd9a0d4f203f107924a0f
argon2008-aiti <macpherson2008@yahoo.com>
2019-02-11 09:28:17 +0000

    debugging

--- agent/forms.py
+++ agent/forms.py
@@ -1,9 +1,12 @@
 from django import forms
 from models import Vendor
+from agent.models import ShopAssistant
+
 
 class VendorForm(forms.ModelForm):
+
     class Meta:
         model = Vendor
-        fields = ['first_name', 'last_name', 'phone', 'hometown', 
+        fields = ['first_name', 'last_name', 'phone', 'hometown',
                   'guarantor', 'relation', 'guarantor_phone',
                   'guarantor_location', 'date_of_birth', 'profile']
--- sales/forms.py
+++ sales/forms.py
@@ -1,39 +1,53 @@
 from django import forms
 from agent.models import Vendor
+from agent.models import ShopAssistant
+
 
 class BookingForm(forms.Form):
-    vendor = forms.ModelChoiceField(Vendor.objects.all())
-    date   = forms.DateField(input_formats=('%d-%m-%Y',))
+    def __init__(self, user=None, **kwargs):
+        all_vendors = Vendor.objects.all()
+        if user:
+            shop = ShopAssistant.objects.filter(user=user).shop
+            all_vendors.filter(shop=shop)
+
+    vendor = forms.ModelChoiceField(all_vendors)
+    date = forms.DateField(input_formats=('%d-%m-%Y',))
+
 
 class UpdateBookingForm(forms.Form):
     vendor = forms.ModelChoiceField(Vendor.objects.all(), disabled=True)
-    date   = forms.DateField(input_formats=('%d-%m-%Y',), disabled=True)
+    date = forms.DateField(input_formats=('%d-%m-%Y',), disabled=True)
+
 
 class ProductBookingForm(forms.Form):
-    product_id = forms.CharField(required=False, widget=forms.HiddenInput()) 
-    product_name = forms.CharField(required=False, widget=forms.HiddenInput()) 
-    product_code = forms.CharField(required=False, widget=forms.HiddenInput()) 
-    booking    = forms.IntegerField(min_value=0)
+    product_id = forms.CharField(required=False, widget=forms.HiddenInput())
+    product_name = forms.CharField(required=False, widget=forms.HiddenInput())
+    product_code = forms.CharField(required=False, widget=forms.HiddenInput())
+    booking = forms.IntegerField(min_value=0)
+
 
 class SalesClosureForm(forms.Form):
-    product_id = forms.CharField(required=False, widget=forms.HiddenInput()) 
-    product_name = forms.CharField(required=False, widget=forms.HiddenInput()) 
-    product_code = forms.CharField(required=False, widget=forms.HiddenInput()) 
-    unit_price = forms.CharField(required=False, widget=forms.HiddenInput()) 
-    booking    = forms.IntegerField(min_value=0, required=False, disabled=True)
-    returns    = forms.IntegerField(min_value=0)
+    product_id = forms.CharField(required=False, widget=forms.HiddenInput())
+    product_name = forms.CharField(required=False, widget=forms.HiddenInput())
+    product_code = forms.CharField(required=False, widget=forms.HiddenInput())
+    unit_price = forms.CharField(required=False, widget=forms.HiddenInput())
+    booking = forms.IntegerField(min_value=0, required=False, disabled=True)
+    returns = forms.IntegerField(min_value=0)
+
 
 class BookingsDetailForm(forms.Form):
-    product_id = forms.CharField(required=False, widget=forms.HiddenInput()) 
-    product_name = forms.CharField(required=False, widget=forms.HiddenInput()) 
-    product_code = forms.CharField(required=False, widget=forms.HiddenInput()) 
-    unit_price = forms.CharField(required=False, widget=forms.HiddenInput()) 
-    booking    = forms.IntegerField(min_value=0, required=False, disabled=True)
-    returns    = forms.IntegerField(min_value=0, required=False, disabled=True)
+    product_id = forms.CharField(required=False, widget=forms.HiddenInput())
+    product_name = forms.CharField(required=False, widget=forms.HiddenInput())
+    product_code = forms.CharField(required=False, widget=forms.HiddenInput())
+    unit_price = forms.CharField(required=False, widget=forms.HiddenInput())
+    booking = forms.IntegerField(min_value=0, required=False, disabled=True)
+    returns = forms.IntegerField(min_value=0, required=False, disabled=True)
+
 
 class BookingPaymentForm(forms.Form):
-    vendor = forms.ModelChoiceField(Vendor.objects.all(), required=False, disabled=True)
-    date   = forms.DateField(input_formats=('%d-%m-%Y',), required=False, disabled=True)
+    vendor = forms.ModelChoiceField(Vendor.objects.all(),
+                                    required=False, disabled=True)
+    date = forms.DateField(input_formats=('%d-%m-%Y',),
+                           required=False, disabled=True)
     sale_total = forms.FloatField(required=False, disabled=True)
     payment_total = forms.FloatField(min_value=0.0)
-
