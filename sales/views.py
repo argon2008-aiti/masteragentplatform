35cc3b8613624662d3864511f869d1d47288487f
argon2008-aiti <macpherson2008@yahoo.com>
2019-02-11 19:13:47 +0000

    debugging

--- sales/views.py
+++ sales/views.py
@@ -56,7 +56,7 @@ class NewBookingView(LoginRequiredMixin, FormView):
         return self.render_to_response(self.get_context_data(formset=formset, booking_form=booking_form))
 
     def post(self, request, *args, **kwargs):
-        booking_form = BookingForm(request.POST, shop_id=None)
+        booking_form = BookingForm(None, request.POST)
 
         ProductBookingFormSet = formset_factory(ProductBookingForm, extra=0)
         formset = ProductBookingFormSet(request.POST)
